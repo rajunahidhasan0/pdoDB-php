228abc7faf7d9ec363e7008f80a4d6259656090d
Nahid Hasan <37169055+rajunahidhasan0@users.noreply.github.com>
2022-01-12 20:56:58 +0600

    Update connection.php

--- database/connection.php
+++ database/connection.php
@@ -1,9 +1,9 @@
 <?php
     function connect(){
         $db = new PDO(
-            "mysql:host=127.0.0.1; dbname=booklist",
-            "root",
-            ""
+            "mysql:host=sql6.freesqldatabase.com; dbname=sql6465183",
+            "sql6465183",
+            "E2tUiYDC19"
         );
         
         $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
