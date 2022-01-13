3433fb09d98b2976e363f55e050d7a7ebf9c4104
Nahid Hasan <37169055+rajunahidhasan0@users.noreply.github.com>
2022-01-13 08:47:38 +0600

    Update connection.php

--- database/connection.php
+++ database/connection.php
@@ -1,20 +1,19 @@
 <?php
-function connect_db()
-{
-    $host = "sql6.freesqldatabase.com";
-    $username = "sql6465178";
-    $password = "wcR7mzwN5k";
-    $dbname = "sql6465178";
-    try {
-        $dsn ="mysql:host=$host;dbname=$dbname";
-        $conn = new PDO($dsn, $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        // $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-        return $conn;
-        
-    } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
+
+    function Connect(){
+        $server_name = "sql6.freesqldatabase.com";
+        $username = "sql6464972";
+        $password = "C1eK6ge5ur";
+        $db_name = "sql6464972";
+        try {
+            $conn = new PDO("mysql:host=$server_name;dbname=$db_name", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+          } catch(PDOException $e) {
+            echo $e;
+            return -1;
+          }
     }
-    return $conn;
-}
+
+?>
